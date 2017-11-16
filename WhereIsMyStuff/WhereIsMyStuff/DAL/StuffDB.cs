0d5dae562ece8e23a1e6344ce7e8df6f353a51ce
Khryssizzle <art.of.alchemy@gmail.com>
2017-11-16 01:57:04 -0500

    Created DAL folder and StuffDB class

---
+++ WhereIsMyStuff/WhereIsMyStuff/DAL/StuffDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WhereIsMyStuff.Models;
+
+namespace WhereIsMyStuff.DAL
+{
+    public class StuffDB :DbContext
+    {
+        public StuffDB() : base("StuffDB")
+        {
+
+        }
+        public DbSet<Entry> Entries { get; set; }
+
+    }
+}
