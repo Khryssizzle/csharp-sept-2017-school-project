f9a2e233966df4741a29c844bc239b6158a38e06
Khryssizzle <art.of.alchemy@gmail.com>
2017-11-16 02:06:12 -0500

    added connection string to web config

--- WhereIsMyStuff/WhereIsMyStuff/DAL/StuffDB.cs
+++ WhereIsMyStuff/WhereIsMyStuff/DAL/StuffDB.cs
@@ -4,6 +4,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using WhereIsMyStuff.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace WhereIsMyStuff.DAL
 {
