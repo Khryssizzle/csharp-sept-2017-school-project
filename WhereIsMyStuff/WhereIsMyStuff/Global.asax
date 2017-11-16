7578745979fe5f00ff107c720f9b7ca33b109899
Khryssizzle <art.of.alchemy@gmail.com>
2017-11-16 01:47:16 -0500

    created entry model

---
+++ WhereIsMyStuff/WhereIsMyStuff/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WhereIsMyStuff.MvcApplication" Language="C#" %>
---
+++ WhereIsMyStuff/WhereIsMyStuff/Models/Entry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WhereIsMyStuff.Models
+{
+    public class Entry
+    {
+
+        [Key]
+        public int Entry_Id { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Date Borrowed")]
+        public DateTime DateBorrowed { get; set; } = DateTime.Now;
+        [Required]
+        [StringLength(100, ErrorMessage = "First Name may not be longer than 100 characters.")]
+        public string Item { get; set; }
+        [Display(Name = "Item Type")]
+        public String ItemType { get; set; }
+        [Display(Name = "Item Condition")]
+        public string ItemCondition { get; set; }
+        public string Notes { get; set; }
+        [Required(ErrorMessage = "First Name is required.")]
+        [StringLength(50, ErrorMessage = "First Name may not be longer than 50 characters.")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
